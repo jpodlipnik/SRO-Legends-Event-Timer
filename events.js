a400d4fe074748f95bff0d1fbcb4fbac14038be6
jaxter-dev <142104703+jaxter-dev@users.noreply.github.com>
2026-07-22 17:28:52 +0200

    Update event times for multiple entries

--- events.js
+++ events.js
@@ -9,15 +9,15 @@ const EVENTS = [
     { id: 6,  name: "Capture The Flag",         times: ["01:00", "05:00", "09:00", "13:00", "17:00", "21:00"],   days: [0, 1, 2, 3, 4, 5, 6] },
     { id: 7,  name: "Battle Arena (Score)",     times: ["04:00", "08:00", "16:00"],                              days: [0, 1, 2, 3, 4, 5, 6] },
     { id: 8,  name: "Battle Arena (Flag)",      times: ["12:00", "19:00"],                                       days: [0, 1, 2, 3, 4, 5, 6] },
-    { id: 9,  name: "Survival Arena",           times: ["11:00", "23:00"],                                       days: [0, 1, 2, 3, 4, 5, 6] },
-    { id: 10, name: "Special Goods Time",       times: ["09:30", "15:30", "21:30"],                              days: [1, 2, 3, 4, 5, 6] },
-    { id: 11, name: "Special Goods Time",       times: ["09:30", "15:30"],                                       days: [0] },
-    { id: 12, name: "Last Man Standing",        times: ["11:30", "23:30"],                                       days: [0, 1, 2, 3, 4, 5, 6] },
+    { id: 9,  name: "Survival Arena",           times: ["10:00", "22:00"],                                       days: [0, 1, 2, 3, 4, 5, 6] },
+    { id: 10, name: "Special Goods Time",       times: ["09:00", "15:00", "21:00"],                              days: [1, 2, 3, 4, 5, 6] },
+    { id: 11, name: "Special Goods Time",       times: ["09:00", "15:00"],                                       days: [0] },
+    { id: 12, name: "Last Man Standing",        times: ["10:30", "22:30"],                                       days: [0, 1, 2, 3, 4, 5, 6] },
     { id: 13, name: "War of Leagues",           times: ["20:00"],                                                days: [0, 1, 2, 3, 4, 5, 6] },
     { id: 14, name: "Tower Defense",            times: ["17:30"],                                                days: [0, 5, 6] },
     { id: 15, name: "Guild Wars",               times: ["20:30"],                                                days: [0] },
     { id: 16, name: "Fortress War",             times: ["21:30"],                                                days: [0] },
     { id: 17, name: "Selket & Neith (S)",       times: ["06:30", "12:30"],                                       days: [0] },
     { id: 18, name: "Isis & Anubis (S)",        times: ["07:30", "14:30"],                                       days: [0] },
-    { id: 19, name: "Thief Special Goods",      times: ["22:00"],                                                days: [0, 1, 2, 3, 4, 5, 6] }
+    { id: 19, name: "Thief Special Goods",      times: ["23:00"],                                                days: [0, 1, 2, 3, 4, 5, 6] }
 ];
